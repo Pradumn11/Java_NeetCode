232f89b7bbe7c4f2ad4f201a8fbb372e17807fe5
Pradumn11 <pradumnyajr@gmail.com>
2023-01-11 21:30:16 +0530

    twosum

---
+++ src/ArraysAndHashing/TwoSum.java
@@ -0,0 +1,23 @@
+package ArraysAndHashing;
+
+import java.util.HashMap;
+
+//Question
+//https://leetcode.com/problems/two-sum/description/
+
+public class TwoSum {
+    public int[] twoSumSolution(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int a=target-nums[i];
+            if(map.containsKey(a)){
+                return new int[]{i,map.get(a)};
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[2];
+    }
+}
