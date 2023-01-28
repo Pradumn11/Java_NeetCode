c09c1628d0c98790fbb423e2184c759c5fe2ca43
Pradumn11 <pradumnyajr@gmail.com>
2023-01-28 21:55:47 +0530

    "Stack MINSTACK 28-01-2023"

---
+++ src/Stack/MinStack.java
@@ -0,0 +1,31 @@
+package Stack;
+
+import java.util.Stack;
+
+public class MinStack {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> min_val = new Stack<Integer>();
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if (min_val.isEmpty() || val <= min_val.peek()) min_val.push(val);
+
+        stack1.push(val);
+    }
+    public void pop() {
+        if (stack1.peek().equals(min_val.peek())) min_val.pop();
+        stack1.pop();
+    }
+
+    public int top() {
+        return stack1.peek();
+    }
+
+    public int getMin() {
+        return min_val.peek();
+    }
+}
+
+
