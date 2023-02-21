424699b7db2dea4fd76702bfb954abc3fe4a30cc
Pradumn11 <pradumnyajr@gmail.com>
2023-02-21 22:26:44 +0530

    "HouseRobber 21-02-2023"

---
+++ src/Array_1_Dynamic/HouseRobber.java
@@ -0,0 +1,15 @@
+package Array_1_Dynamic;
+
+public class HouseRobber {
+    public int houseRobberSolution(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < n; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        return dp[n];
+    }
+}
